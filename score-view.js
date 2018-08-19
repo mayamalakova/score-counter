ef4e0fb0e16e0aee39350b4cb9b72659cb75d86b
Maya <maya.malakova@gmail.com>
2018-08-19 10:17:43 +0100

    Rename scoreLeft, scoreRight

--- score-view.js
+++ score-view.js
@@ -1,15 +1,15 @@
 var app = new Vue({ 
     el: '#content',
     data: {
-        score1: 0,
-        score2: 0,
+        scoreLeft: 0,
+        scoreRight: 0,
         games1: 0,
         games2: 0,
         gameScores: []
     },
     methods: {
         increaseLeft: function () {
-            this.score1++;
+            this.scoreLeft++;
             if (this.leftWinsGame()) {
                 this.games1++;
                 this.finishGame();
@@ -17,13 +17,13 @@ var app = new Vue({
         },
 
         decreaseLeft: function () {
-            if (this.score1 > 0) {
-                this.score1--;
+            if (this.scoreLeft > 0) {
+                this.scoreLeft--;
             }
         },
 
         increaseRight: function() {
-            this.score2++;
+            this.scoreRight++;
             if (this.rightWinsGame()) {
                 this.games2++;
                 this.finishGame();
@@ -31,27 +31,27 @@ var app = new Vue({
         },
 
         decreaseRight: function() {
-            if (this.score2 > 0) {
-                this.score2--;
+            if (this.scoreRight > 0) {
+                this.scoreRight--;
             }
         },
 
         resetScore: function() {
-            this.score1 = 0;
-            this.score2 = 0;
+            this.scoreLeft = 0;
+            this.scoreRight = 0;
         },
 
         finishGame: function() {
-            this.gameScores.push({left: this.score1, right: this.score2});
+            this.gameScores.push({left: this.scoreLeft, right: this.scoreRight});
             this.resetScore();
         },
 
         leftWinsGame: function() {
-            return this.score1 >= 11 && this.score1 - this.score2 > 1;
+            return this.scoreLeft >= 11 && this.scoreLeft - this.scoreRight > 1;
         },
 
         rightWinsGame: function() {
-            return this.score2 >= 11 && this.score2 - this.score1 > 1
+            return this.scoreRight >= 11 && this.scoreRight - this.scoreLeft > 1
         }
         
       }
