1b4549d0453878a4e6f857072a51d6ae2368a5a3
Maya <maya.malakova@gmail.com>
2018-08-19 15:22:17 +0100

    Try to use node-static as a web server

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-var express = require("express");
-var path    = require("path");
-var app     = express();
-
-app.get('/',function(req,res){
-     res.sendFile(path.join(__dirname+'/score-view.html'));
-});
-
-app.get('/score-view.js',function(req,res){
-     res.sendFile(path.join(__dirname+'/score-view.js'));
-});
-
-app.get('/score-footer.js',function(req,res){
-     res.sendFile(path.join(__dirname+'/score-footer.js'));
-});
-
-app.get('/score-view.css',function(req,res){
-     res.sendFile(path.join(__dirname+'/score-view.css'));
-});
-
-app.listen(3000);
-
-console.log("Running at Port 3000");
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
