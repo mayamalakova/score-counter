c2446a510cbddb264802af78dbe31c82f6f0d667
Maya <maya.malakova@gmail.com>
2018-08-20 18:54:32 +0100

    Style it up

--- game-summary.js
+++ game-summary.js
@@ -2,7 +2,7 @@ Vue.component('game-summary', {
     props: ['gameWinner', 'playerLeft', 'playerRight', 'gameScores'],
     template: `
     <div class='score-summary' >
-        <div>{{this.gameWinner}} wins game</div>
+        <div class='game-winner'>{{this.gameWinner}} wins game</div>
         <score-footer  :player-left='playerLeft' :player-right='playerRight' :game-scores='gameScores'></score-footer>
      </div>
      `
