a504740eff72d0635b3db6bc2b8d30dd6030a360
Maya <maya.malakova@gmail.com>
2018-08-20 19:04:10 +0100

    Update the text

--- game-summary.js
+++ game-summary.js
@@ -2,7 +2,7 @@ Vue.component('game-summary', {
     props: ['gameWinner', 'playerLeft', 'playerRight', 'gameScores'],
     template: `
     <div class='score-summary' >
-        <div class='game-winner'>{{this.gameWinner}} wins game</div>
+        <div class='game-winner'>{{this.gameWinner}} wins the game!</div>
         <score-footer  :player-left='playerLeft' :player-right='playerRight' :game-scores='gameScores'></score-footer>
      </div>
      `
