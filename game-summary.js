67c1e1a14b2c90bac716bd77b60c760e0f755852
Maya <maya.malakova@gmail.com>
2018-08-20 19:43:58 +0100

    Show winner on the same page

--- game-summary.js
+++ game-summary.js
@@ -1,10 +1,6 @@
 Vue.component('game-summary', {
-    props: ['gameWinner', 'playerLeft', 'playerRight', 'gameScores'],
+    props: ['gameWinner'],
     template: `
-    <div class='score-summary' >
         <div class='game-winner'>{{this.gameWinner}} wins the game!</div>
-        <score-footer  :player-left='playerLeft' :player-right='playerRight' :game-scores='gameScores'></score-footer>
-        <button v-on:click="$emit('next-game')" class="button-next">Next game</button>
-     </div>
      `
 });
