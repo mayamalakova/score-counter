1c38d63372e5664db95663bd8f35e500b2cf9203
Maya Malakova <maya.malakova@gmail.com>
2018-09-07 16:04:12 +0100

    Restructure the components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        <h1>Hello World!</h1>
+        <game-progress
+                :score-left="scoreLeft" :score-right="scoreRight" :server="server"
+                v-on:increase-left="increaseLeft"
+                v-on:decrease-left="decreaseLeft"
+                v-on:increase-right="increaseRight"
+                v-on:decrease-right="decreaseRight"></game-progress>
     </div>
 </template>
+<script>
+    import gameProgress from './game-progress.vue';
+
+    const App = {
+        components: {
+            'game-progress': gameProgress
+        }
+    }
+
+    export default App;
+</script>
---
+++ src/game-progress.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="score-container" >
+        <div class="score-left" v-on:click="$emit('increase-left')">
+            <div class="score-val">
+                <span class="score-val-number">{{scoreLeft}} </span>
+                <span class="serve-indicator">{{server === 'left' ? "'" : ""}}</span>
+            </div>
+            <div class="btn-minus" v-on:click.stop="$emit('decrease-left')">-</div>
+        </div>
+        <div class="score-right" v-on:click="$emit('increase-right')">
+            <div class="score-val">
+                <span class="score-val-number">{{scoreRight}} </span>
+                <span class="serve-indicator">{{server === 'right' ? "'" : ""}}</span>
+            </div>
+            <div class="btn-minus" v-on:click.stop="$emit('decrease-right')">-</div>
+        </div>
+    </div>
+</template>
+<script>
+const gameProgress = {
+    props: ['scoreLeft', 'scoreRight','server']
+};
+
+export default gameProgress;
+</script>
