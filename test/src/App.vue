9c86e20bb6a4ad12edfde7a5041f1b98f521c0a0
Maya <maya.malakova@gmail.com>
2018-09-15 09:55:27 +0100

    Add style test harness

---
+++ test/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <div class="score-l">11</div>
+        <div class="score-r">11</div>
+    </div>
+</template>
+<script>
+import './app.styl';
+export default {
+    test: 'best'
+}
+</script>
+
---
+++ test/webpack.config.dev.js
@@ -0,0 +1,42 @@
+'use strict'
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/test.js'
+    ],
+
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+
+            {
+                test: /\.styl(us)?$/,
+                loaders: [ 'style-loader', 'css-loader', 'stylus-loader' ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+}
