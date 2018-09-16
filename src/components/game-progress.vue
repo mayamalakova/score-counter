0902621ef0987815010f1eb4c4c4f7cdef829473
Maya <maya.malakova@gmail.com>
2018-09-16 12:02:51 +0100

    Adjust font when there is a dougle digit score

--- src/components/game-progress.vue
+++ src/components/game-progress.vue
@@ -2,14 +2,14 @@
     <div class="score-container" >
         <div class="score-left" v-on:click="$emit('increase-left')">
             <div class="score-val">
-                <span class="score-val-number">{{scoreLeft}} </span>
+                <span :class="valClass">{{scoreLeft}} </span>
                 <span class="serve-indicator">{{server === 'left' ? "'" : ""}}</span>
             </div>
             <div class="btn-minus" v-on:click.stop="$emit('decrease-left')">-</div>
         </div>
         <div class="score-right" v-on:click="$emit('increase-right')">
             <div class="score-val">
-                <span class="score-val-number">{{scoreRight}} </span>
+                <span :class="valClass">{{scoreRight}} </span>
                 <span class="serve-indicator">{{server === 'right' ? "'" : ""}}</span>
             </div>
             <div class="btn-minus" v-on:click.stop="$emit('decrease-right')">-</div>
@@ -18,7 +18,13 @@
 </template>
 <script>
 export default {
-    props: ['scoreLeft', 'scoreRight','server']
+    props: ['scoreLeft', 'scoreRight','server'],
+    computed: {
+        valClass: function() {
+            let valueSize = (this.scoreLeft > 9 || this.scoreRight > 9) ? "small" : "normal";
+            return "score-val-number " + valueSize;
+        }
+  }
 };
 
 </script>
