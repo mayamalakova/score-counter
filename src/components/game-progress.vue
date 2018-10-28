bc80bc429137b398e0069c6033c81b7a569c6547
Maya.Malakova <maya.malakova@gmail.com>
2018-10-28 09:59:34 +0000

    Separate next and finish buttons

--- src/components/game-progress.vue
+++ src/components/game-progress.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="score-container" >
+    <div class="score-container">
         <div class="score-left" @click="$emit('increase-left')">
             <div class="score-val">
                 <span :class="valClass">{{scoreLeft}} </span>
@@ -11,37 +11,40 @@
         <div class="score-right" @click="$emit('increase-right')">
             <div class="score-val">
                 <span :class="valClass">{{scoreRight}} </span>
-                
+
                 <span class="serve-indicator">{{server === 'right' ? "'" : ""}}</span>
             </div>
             <div class="btn-minus" @click.stop="$emit('decrease-right')">-</div>
         </div>
         <div :class="overlayClass">
             <div class="btn-continue" @click.stop="$emit('new-game')">
-            <div>{{overlayButtonLabel}}</div>
-            <div class="icon-arrow-right"> </div>
+                <div>{{overlayButtonLabel}}</div>
+                <div class="icon-arrow-right"></div>
+            </div>
+        </div>
+        <div class="cover-all" v-if="$parent.gameWinner && !$parent.matchWinner">
+            <div class="btn-continue" @click.stop="$emit('new-game')">
+                <div>Next</div>
+                <div class="icon-arrow-right"></div>
+            </div>
+        </div>
+        <div class="cover-all" v-if="$parent.matchWinner">
+            <div class="btn-continue" @click.stop="$emit('new-game')">
+                <div>Finish</div>
+                <div class="icon-arrow-right"></div>
             </div>
         </div>
     </div>
 </template>
 <script>
-export default {
-    props: ['scoreLeft', 'scoreRight','server'],
-    computed: {
-        valClass: function() {
-            let valueSize = (this.scoreLeft > 9 || this.scoreRight > 9) ? "small" : "normal";
-            return "score-val-number " + valueSize;
-        },
-
-        overlayClass: function() {
-            let hidden = this.$parent.gameWinner ? "" : " hidden";
-            return "cover-all" + hidden;
-        },
-
-        overlayButtonLabel: function() {
-            return this.$parent.matchWinner ? "Finish" : "Next";
+    export default {
+        props: ['scoreLeft', 'scoreRight', 'server'],
+        computed: {
+            valClass: function () {
+                let valueSize = (this.scoreLeft > 9 || this.scoreRight > 9) ? "small" : "normal";
+                return "score-val-number " + valueSize;
+            }
         }
-  }
-};
+    };
 
 </script>
