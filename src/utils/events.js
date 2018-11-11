79783067a91dc331b8ef1de636852e654c9c58ba
Maya.Malakova <maya.malakova@gmail.com>
2018-11-11 14:09:41 +0000

    use constants for event names

---
+++ src/utils/events.js
@@ -0,0 +1,7 @@
+const INCREASE_LEFT = 'increase-left';
+const DECREASE_LEFT = 'decrease-left';
+const INCREASE_RIGHT = 'increase-right';
+const DECREASE_RIGHT = 'decrease-right';
+const NEW_GAME = 'new-game';
+
+export default {INCREASE_LEFT, INCREASE_RIGHT, DECREASE_LEFT, DECREASE_RIGHT, NEW_GAME};
