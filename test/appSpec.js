e70601be0e4fb96caa5fb45b96d72007e208d7a1
Maya.Malakova <maya.malakova@gmail.com>
2018-12-02 10:49:12 +0000

    add a mocha test

---
+++ test/appSpec.js
@@ -0,0 +1,8 @@
+describe("The table tennis scoring app", function () {
+
+    describe('Game progress', function () {
+        it('should show left and right scores', function () {
+
+        });
+    });
+});
